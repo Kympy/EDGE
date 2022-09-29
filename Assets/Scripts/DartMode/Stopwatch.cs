1bae32a18b8edf463ca759b59a5814e41b1a3ae1
MinJ00choi <111823130+MinJ00choi@users.noreply.github.com>
2022-09-29 17:39:29 +0900

    DartGame
    불필요한 파일 삭제

---
+++ Assets/Scripts/DartMode/Stopwatch.cs
@@ -0,0 +1,62 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class Stopwatch : MonoBehaviour
+{
+    public Text text_Timer;
+    float time_start;
+    float time_current;
+    float time_Max = 10f;
+    bool isEnded;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Reset_Timer();    
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isEnded)
+            return;
+        
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Debug.Log("스톱워치 시작");
+            Check_Timer();
+        }
+    }
+
+    void Check_Timer()
+    {
+        time_current = Time.time - time_start;
+        if (time_current < time_Max)
+        {
+            text_Timer.text = $"{time_current:N2}";
+        }
+
+        else if (!isEnded)
+        {
+            End_Timer();
+        }
+    }
+
+    void End_Timer()
+    {
+        time_current = time_Max;
+        text_Timer.text = $"{time_current:N2}";
+        isEnded = true;
+    }
+
+    void Reset_Timer()
+    {
+        time_start = Time.time;
+        time_current = 0;
+        text_Timer.text = $"{time_current:N2}";
+        isEnded = false;
+    }
+}
